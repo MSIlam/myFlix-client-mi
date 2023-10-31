58558256b1c84a151e3f8b68db252267b5a1607b
Muhammad88IT <saiful.zoology@yahoo.com>
2023-10-31 23:50:59 +0100

    Setting up prop in movie-view with propType

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,29 +1,52 @@
+import PropTypes from "prop-types";
+
 export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div className="movie-details">
       <div className="movie-image">
         <img
-          src={movie.image}
-          alt={movie.title}
+          src={movie.ImageURL}
+          alt={movie.Title}
           style={{ width: "200px", height: "300px" }}
         />
       </div>
       <div className="movie-info">
-        <h2>{movie.title}</h2>
+        <h2>{movie.Title}</h2>
         <p>
-          <strong>Description:</strong> {movie.description}
+          <strong>Description:</strong> {movie.Description}
         </p>
         <p>
-          <strong>Genres:</strong> {movie.genres.name}
+          <strong>Genres:</strong> {movie.Genres.Name}
         </p>
         <p>
-          <strong>Director:</strong> {movie.director.name}
+          <strong>Director:</strong> {movie.Director.Name}
         </p>
         <p>
-          <strong>Release year:</strong> {movie.year}
+          <strong>Release year:</strong> {movie.Year}
         </p>
       </div>
       <button onClick={onBackClick}>Back</button>
     </div>
   );
+};
+
+// Proptype conditions
+MovieView.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    ImageURL: PropTypes.string,
+    Description: PropTypes.string,
+    Genres: PropTypes.shape({
+      Name: PropTypes.string,
+      Description: PropTypes.string,
+    }),
+    Director: PropTypes.shape({
+      Name: PropTypes.string,
+      Bio: PropTypes.string,
+      Birthyear: PropTypes.string,
+    }),
+    Stars: PropTypes.string,
+    Year: PropTypes.string,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
