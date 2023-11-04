c67213ba068abecbad29d87c409d6604ef215446
Muhammad88IT <saiful.zoology@yahoo.com>
2023-11-04 23:43:11 +0100

    Modification of mainview for signup and login including JWT authentication

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,20 +1,29 @@
-import { useState } from "react";
+import React from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-import { useEffect } from "react";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem("user"));
+  const storedToken = localStorage.getItem("token");
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(storedUser);
+  // const [token, setToken] = useState (null);
 
   // Hooking
   useEffect(() => {
-    fetch("https://myflix-mi-e89972ef7472.herokuapp.com/movies")
+    if (!user || !storedToken) {
+      return;
+    }
+    fetch("https://myflix-mi-e89972ef7472.herokuapp.com/movies", {
+      headers: { Authorization: `Bearer ${storedToken}` },
+    })
       .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        const moviesFromApi = data.map((movie) => {
+      .then((movies) => {
+        const moviesFromApi = movies.map((movie) => {
           return {
             id: movie._id,
             Title: movie.Title,
@@ -32,10 +41,29 @@ export const MainView = () => {
             Year: movie.Year,
           };
         });
-
         setMovies(moviesFromApi);
       });
-  }, []);
+  }, [user, storedToken]);
+
+  if (!user) {
+    return (
+      <React.Fragment>
+        <p>
+          <strong>Login</strong>
+        </p>
+        <LoginView
+          onLoggedIn={(user, token) => {
+            setUser(user);
+            localStorage.setItem("token", token);
+          }}
+        />
+        <p>
+          <strong>Signup</strong>
+        </p>
+        <SignupView />
+      </React.Fragment>
+    );
+  }
 
   if (selectedMovie) {
     return (
@@ -47,20 +75,34 @@ export const MainView = () => {
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
+    return <div> The list is empty! </div>;
   }
 
   return (
     <div>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
+      <div>
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            movie={movie}
+            onMovieClick={(newSelectedMovie) =>
+              setSelectedMovie(newSelectedMovie)
+            }
+          />
+        ))}
+      </div>
+      <div>
+        <button
+          onClick={() => {
+            // setUser(null); setToken(null); localStorage.clear(); }}> Logout </button>
+            setUser(null);
+            localStorage.removeItem("token");
+            localStorage.removeItem("user");
           }}
-        />
-      ))}
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 };
