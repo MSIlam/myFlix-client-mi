967d0b24047d6da82e8ed970713cb7488252f4f8
Muhammad88IT <saiful.zoology@yahoo.com>
2023-11-06 12:40:16 +0100

    Added bootstrap card style

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,24 +1,26 @@
 import PropTypes from "prop-types";
-
+import { Button, Card, CardBody } from "react-bootstrap";
+import "./movie-card.scss";
 import React from "react";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
-    <div
-      className="movie-card" // Add a class for styling if needed onClick=
-      onClick={() => {
-        onMovieClick(movie);
-      }}
-    >
-      {movie.Title}
-    </div>
+    <Card onClick={() => onMovieClick(movie)} variant="link" className="h-100">
+      <Card.Img variant="top" src={movie.ImageURL} style={{ width: "100%" }} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Year}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
 // defining the prop constrains
 MovieCard.propTypes = {
   movie: PropTypes.shape({
+    ImageURL: PropTypes.string,
     Title: PropTypes.string.isRequired,
+    Year: PropTypes.string,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
 };
